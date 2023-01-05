10a23d0bde47a3f78fe80cbe619a4bbbeac673f4
Gaboza18 <ssychopagi@naver.com>
2023-01-05 16:47:12 +0900

    1.8 Nicknames part Two

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,46 @@
+const messageList = document.querySelector("ul");
+const nicknameForm = document.querySelector("#nickname"); // 닉네임
+const messageForm = document.querySelector("#message"); // 메세지
 const socket = new WebSocket(`ws://${window.location.host}`); // ' (x) -> `(o)
 
+
+function makeMessage(type, payload) {
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+}
+
 socket.addEventListener("open", () => {
-    console.log("브라우저 연결 성공!");
+    console.log("연결 성공!");
 });
 
 socket.addEventListener("message", (message) => {
-    console.log("새로운 메세지: ", message.data);
+    // console.log("새로운 메세지: ", message.data);
+
+    // 화면에 메세지 내용 표시
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
-    console.log("브라우저 연결 끊김!");
+    console.log("연결 끊김!");
 });
 
-setTimeout(() => {
-    socket.send("안녕하세요");
-}, 10000); // 10초
+// 메세지 입력
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value = ""; // 메세지 입력칸 초기화
+}
+
+// 닉네임 입력
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nicknameForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+    input.value = ""; // 닉네임 입력칸 초기화
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nicknameForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -17,13 +17,44 @@ const handleListen = () => console.log('Listening on http://localhost:3000'); //
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = []; // 서버에 연결한 (socket)값을 브라우저를 저장한다
+
 wss.on("connection", (socket) => {
-    console.log("브라우저 연결 성공!");
-    socket.on("close", () => console.log("브라우저 연결 끊김!"));
-    socket.on("message", (message) => {
-        console.log(message.toString('utf-8')); // 한글 오류
+
+    sockets.push(socket);
+    socket["nickname"] = "익명";
+    console.log("연결 성공!");
+
+    socket.on("close", () => {
+        console.log("연결 끊김!"); // 익명함수
     });
-    socket.send("Hello~!!");
+
+    // 자신이 보낸 메세지를 console(개발자 도구) 에서 확인
+    socket.on("message", (msg) => {
+
+        const message = JSON.parse(msg); // String 값
+
+        // 서버에 입력한 값
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => 
+                    aSocket.send(`${socket.nickname}: ${message.payload}`)); // 닉네임: 메세지 내용 출력
+            case "nickname":
+                socket["nickname"] = message.payload; // 닉네임
+        }
+        // socket.send(message.toString('utf-8')); // 한글 깨짐 해결 message.toString('utf-8')
+    });
 });
 
 server.listen(3000, handleListen);
+
+{
+    type: "message"
+    payload: "hello everyone!"
+}
+
+
+{
+    type: "nickname"
+    payload: "Gaboza18"
+}
